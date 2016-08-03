7645f2efd94ae5a5678eb36350b1c96cead25b49
Wisen Tanasa <wisen@ceilfors.com>
2016-08-03 08:54:42 +0100

    Add OpenGrok download url to attribute.

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,6 @@ default['java']['install_flavor'] = 'oracle'
 default['java']['jdk_version'] = '8'
 default['java']['set_etc_environment'] = true
 default['java']['oracle']['accept_oracle_download_terms'] = true
+
+default['opengrok']['download_url'] = 'https://github.com/OpenGrok/OpenGrok/files/213268/opengrok-0.12.1.5.tar.gz'
+default['opengrok']['checksum'] = 'c3ce079f6ed1526c475cb4b9a7aa901f75507318c93b436d6c14eba4098e4ead'
