dc8631759b36481f33ac93cad2eb4c334091ffb3
Wisen Tanasa <wisen@ceilfors.com>
2016-08-15 08:09:10 +0100

    Move color option from .rspec to spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'chefspec'
 require 'chefspec/berkshelf'
 
 RSpec.configure do |config|
+  config.color = true
   config.filter_run focus: true
   config.run_all_when_everything_filtered = true
 end
