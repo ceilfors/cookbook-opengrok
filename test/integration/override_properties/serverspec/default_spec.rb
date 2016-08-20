1b99ab9fcb505128684527c2e1f51f023e6710cd
Wisen Tanasa <wisen@ceilfors.com>
2016-08-20 08:53:10 +0100

    Move back to ServerSpec due to chef/kitchen-inspec#97 and chef/inspec#514.

---
+++ test/integration/override_properties/serverspec/default_spec.rb
@@ -0,0 +1,60 @@
+require 'spec_helper'
+
+describe 'opengrok overridden properties' do
+
+  describe command('printenv JAVA_HOME') do
+    its(:exit_status) { should eq 0 }
+  end
+
+  describe user('custom_user') do
+    it { should exist }
+  end
+
+  describe group('custom_group') do
+    it { should exist }
+  end
+
+  describe file('/opt/tomcat_opengrok/LICENSE') do
+    it { should be_file }
+    it { should be_owned_by 'custom_user' }
+    it { should be_grouped_into 'custom_group' }
+  end
+
+  describe service('tomcat_opengrok') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe file('/opt/custom/opengrok/bin/OpenGrok') do
+    it { should be_file }
+    it { should be_owned_by 'custom_user' }
+    it { should be_grouped_into 'custom_group' }
+  end
+
+  describe file('/opt/tomcat_opengrok/webapps/source.war') do
+    it { should be_file }
+    it { should be_owned_by 'custom_user' }
+  end
+
+  describe command('curl http://localhost:8080/source/') do
+    its(:stdout) { should match %r(<title>Search</title>) }
+  end
+
+  %w(src data etc log).each do |dir|
+    describe file("/var/custom/opengrok/#{dir}") do
+      it { should be_directory }
+      it { should be_owned_by 'custom_user' }
+    end
+  end
+
+  describe file('/var/custom/opengrok/etc/configuration.xml') do
+    it { should be_file }
+    it { should be_owned_by 'custom_user' }
+  end
+
+  describe file('/var/custom/opengrok/logging.properties') do
+    it { should be_file }
+    it { should be_owned_by 'custom_user' }
+  end
+
+end
