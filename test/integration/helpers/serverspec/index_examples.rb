3a5c56fa1c3e43eaaa34fc89ea9fae3af9b9fab5
Wisen Tanasa <wisen@ceilfors.com>
2016-09-14 07:44:42 +0100

    Split its(:content) as it doesn't handle multile matchers inside.

--- test/integration/helpers/serverspec/index_examples.rb
+++ test/integration/helpers/serverspec/index_examples.rb
@@ -20,15 +20,11 @@ shared_examples_for 'opengrok index' do |args = {}|
     it { should be_grouped_into expected_group }
     it { should be_owned_by expected_user }
     it { should be_executable }
-    its(:content) do
-      # TODO It seems like ServerSpec output is not showing nicely when we have multiple 'should match'
-      # The output shows as if we only have 1 test case which make it not really clear for the user.
-      should match /#{expected_java_opts}/
-      should match %r(-Djava.util.logging.config.file=#{expected_home_path}/logging.properties)
-      should match %r(-jar #{expected_install_path}/opengrok/lib/opengrok.jar)
-      should match %r(-R #{expected_home_path}/etc/configuration.xml)
-      should match /#{expected_extra_opts}/
-    end
+    its(:content) { should match /#{expected_java_opts}/ }
+    its(:content) { should match %r(-Djava.util.logging.config.file=#{expected_home_path}/logging.properties) }
+    its(:content) { should match %r(-jar #{expected_install_path}/opengrok/lib/opengrok.jar) }
+    its(:content) { should match %r(-R #{expected_home_path}/etc/configuration.xml) }
+    its(:content) { should match /#{expected_extra_opts}/ }
   end
 
   describe file("#{expected_home_path}/data/index") do
