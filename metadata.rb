75b5718b9f06485a4f23f5d87c9195881198dc0d
Wisen Tanasa <wisen@ceilfors.com>
2016-09-25 09:39:25 +0100

    Bump version to 0.2.0.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'wisen@ceilfors.com'
 license 'Apache 2.0'
 description 'Installs/Configures OpenGrok'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.2.0'
 issues_url 'https://github.com/ceilfors/cookbook-opengrok/issues'
 source_url 'https://github.com/ceilfors/cookbook-opengrok'
 
